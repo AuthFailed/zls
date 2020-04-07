154d58220212e94046b72891388a5cd0c7f1d00d
Roman Chursanov <authfailed@gmail.com>
2020-04-07 14:06:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,30 +8,27 @@ echo "      			  / / | |     \___ \ ";
 echo "       			 / /__| |____ ____) |";
 echo "      			/_____|______|_____/ ";
 echo "                                       ";
-echo "         ArchLinux install script ";
+echo "         Скрипт установки ArchLinux ";
 echo "";
 echo "";
 
-# Syncing system datetime
+# Синхронизация даты
 timedatectl set-ntp true
 
-# Getting latest mirrors for italy and germany
-wget -O mirrorlist "https://www.archlinux.org/mirrorlist/?country=DE&country=IT&protocol=https&ip_version=4"
+# Установка зеркал с России
+wget -O mirrorlist "https://www.archlinux.org/mirrorlist/?country=RU&protocol=https&ip_version=4"
 sed -ie 's/^.//g' ./mirrorlist
 mv ./mirrorlist /etc/pacman.d/mirrorlist
 
-# Updating mirrors
+# Обновление зеркал
 pacman -Syyy
 
-# Installs FZF
+# Установка FZF
 pacman -S --noconfirm fzf
 
-# Choose which type of install you're going to use
-install_type=$(printf "Intel\nAMD" | fzf)
-
-# Choose which disk you wanna use
+# Выбор диска для использования
 disk=$(sudo fdisk -l | grep 'Disk /dev/' | awk '{print $2,$3,$4}' | sed 's/,$//' | \
-fzf --preview 'echo -e "Choose the disk you want to use.\nKeep in mind it will follow this rules:\n\n500M: boot partition\n100G: root partition\nAll remaining space for home partition"' | \
+fzf --preview 'echo -e "Выберите диск для использования.\nПомните о следующем:\n\n500M: раздел /boot\n100G: раздел /root\nВсё оставшееся место будет использовано под /home"' | \
 sed -e 's/\/dev\/\(.*\):/\1/' | awk '{print $1}')
 
 # Formatting disk
@@ -58,108 +55,61 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/$disk
   w # writing changes to disk
 EOF
 
-# Outputting partition changes
+# Вывод изменений в разметке
 fdisk -l /dev/$disk
 
-# partition filesystem formatting
+# форматирование файловой системы раздела
 yes | mkfs.fat -F32 /dev/${selected_disk}1
 yes | mkfs.ext4 /dev/${selected_disk}2
 yes | mkfs.ext4 /dev/${selected_disk}3
 
-# disk mount
+# монтирование диска
 mount /dev/${selected_disk}3 /mnt
 mkdir /mnt/boot
 mkdir /mnt/home
 mount /dev/${selected_disk}1 /mnt/boot
 mount /dev/${selected_disk}2 /mnt/home
 
-# Choosing desktop environment
-de=$(printf "Deepin\ni3\nGNOME" | fzf)
+# установка стандартных пакетов
+pacstrap /mnt base base-devel vim grub networkmanager \
+git zsh amd-ucode curl xorg xorg-server go tlp termite \
+xorg-xinit dialog nvidia nvidia-settings wget bmon \
+pulseaudio pamixer light feh rofi neofetch xorg-xrandr \
+kitty libsecret gnome-keyring libgnome-keyring dnsutils \
+os-prober efibootmgr ntfs-3g unzip wireless_tools ccache \
+iw wpa_supplicant iwd ppp dhcpcd netctl linux linux-firmware \
+linux-headers picom xf86-video-intel mesa bumblebee powertop \
+gtk3 lightdm lightdm-webkit2-greeter lightdm-webkit2-greeter-litarvan
 
-# Pacstrap-ping
-if [ $install_type = "Intel" ]; then
-	if [ $de = "i3" ]; then
-  		pacstrap /mnt base base-devel vim grub networkmanager \
-  		git zsh intel-ucode curl xorg xorg-server go tlp termite \
-  		xorg-xinit dialog nvidia nvidia-settings wget bmon \
-  		pulseaudio pamixer light feh rofi neofetch xorg-xrandr \
-  		kitty libsecret gnome-keyring libgnome-keyring dnsutils \
-  		os-prober efibootmgr ntfs-3g unzip wireless_tools ccache \
-  		iw wpa_supplicant iwd ppp dhcpcd netctl linux linux-firmware \
-  		linux-headers picom xf86-video-intel mesa bumblebee powertop \
-  		gtk3 lightdm lightdm-webkit2-greeter lightdm-webkit2-greeter-litarvan
-	elif [ $de = "GNOME" ]; then
-		pacstrap /mnt base base-devel vim grub networkmanager \
-  		git zsh intel-ucode curl xorg xorg-server go tlp ccache \
-  		xorg-xinit dialog nvidia nvidia-settings wget \
-		pulseaudio neofetch xorg-xrandr kitty os-prober ntfs-3g \
-		efibootmgr unzip wireless_tools iw wpa_supplicant iwd ppp dhcpcd netctl \
-		linux linux-firmware linux-headers mesa gtk3 gnome gnome-extra gdm
-	else
-		pacstrap /mnt base base-devel vim grub networkmanager \
-  		git zsh intel-ucode curl xorg xorg-server go tlp ccache \
-  		xorg-xinit dialog nvidia nvidia-settings wget \
-		pulseaudio neofetch xorg-xrandr kitty os-prober ntfs-3g \
-		efibootmgr unzip wireless_tools iw wpa_supplicant iwd ppp dhcpcd netctl \
-		linux linux-firmware linux-headers mesa gtk3 lightdm deepin deepin-extra
-	fi
-else
-	if [ $de = "i3" ]; then
-		pacstrap /mnt base base-devel vim grub networkmanager \
-  		git zsh amd-ucode curl xorg xorg-server go tlp termite \
-  		xorg-xinit dialog nvidia nvidia-settings wget bmon \
-  		pulseaudio pamixer light feh rofi neofetch xorg-xrandr \
-  		kitty libsecret gnome-keyring libgnome-keyring dnsutils \
-  		os-prober efibootmgr ntfs-3g unzip wireless_tools ccache \
-  		iw wpa_supplicant iwd ppp dhcpcd netctl linux linux-firmware \
-  		linux-headers picom xf86-video-intel mesa bumblebee powertop \
-  		gtk3 lightdm lightdm-webkit2-greeter lightdm-webkit2-greeter-litarvan
-	elif [ $de = "GNOME" ]; then
-		pacstrap /mnt base base-devel vim grub networkmanager \
-  		git zsh amd-ucode curl xorg xorg-server go tlp ccache \
-  		xorg-xinit dialog nvidia nvidia-settings wget \
-		pulseaudio neofetch xorg-xrandr kitty os-prober ntfs-3g \
-		efibootmgr unzip wireless_tools iw wpa_supplicant iwd ppp dhcpcd netctl \
-		linux linux-firmware linux-headers mesa gtk3 gnome gnome-extra gdm
-	else
-		pacstrap /mnt base base-devel vim grub networkmanager \
-  		git zsh amd-ucode curl xorg xorg-server go tlp ccache \
-  		xorg-xinit dialog nvidia nvidia-settings wget \
-		pulseaudio neofetch xorg-xrandr kitty os-prober ntfs-3g \
-		efibootmgr unzip wireless_tools iw wpa_supplicant iwd ppp dhcpcd netctl \
-		linux linux-firmware linux-headers mesa gtk3 lightdm deepin deepin-extra
-	fi
-fi
-
-# Generating fstab
+# Генерация fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Updating repo status
+# Обновление статуса репозиториев
 arch-chroot /mnt pacman -Syyy
 
-# Setting right timezone
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+# Установка таймзоны (+7)
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
 
-# Enabling font presets for better font rendering
+# Подключение пресетов шрифтов для лучшего рендера
 arch-chroot /mnt ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
 arch-chroot /mnt ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 arch-chroot /mnt ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 
-# Synchronizing timer
+# Синхронизация времени
 arch-chroot /mnt hwclock --systohc
 
-# Localizing system
-arch-chroot /mnt sed -ie 's/#it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/g' /etc/locale.gen
-arch-chroot /mnt sed -ie 's/#it_IT ISO-8859-1/it_IT ISO-8859-1/g' /etc/locale.gen
+# Локализация системы
+arch-chroot /mnt sed -ie 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+arch-chroot /mnt sed -ie 's/#en_US ISO-8859-1/en_US ISO-8859-1/g' /etc/locale.gen
 
-# Generating locale
+# Генерация локалей
 arch-chroot /mnt locale-gen
 
-# Setting system language
-arch-chroot /mnt echo "LANG=it_IT.UTF-8" >> /mnt/etc/locale.conf
+# Установка языка системы
+arch-chroot /mnt echo "LANG=en_US.UTF-8" >> /mnt/etc/locale.conf
 
-# Choose machine name
-arch-chroot /mnt read -p "Choose your machine name (only one word):" machine
+# Установка названия машины
+arch-chroot /mnt read -p "Введите название машины (только одно слово):" machine
 
 # Setting machine name
 arch-chroot /mnt echo ${machine} >> /mnt/etc/hostname
@@ -173,7 +123,7 @@ arch-chroot /mnt echo "127.0.1.1 ${machine}.localdomain ${machine}" >> /mnt/etc/
 arch-chroot /mnt sed -ie 's/# %wheel ALL=(ALL)/%wheel ALL=(ALL)/g' /etc/sudoers
 
 # Choose your username
-arch-chroot /mnt read -p "Insert your username (only one word):" username
+arch-chroot /mnt read -p "Введите username (только одно слово):" username
 
 # Make initframs
 arch-chroot /mnt mkinitcpio -p linux
@@ -196,29 +146,15 @@ arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootlo
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # Making services start at boot
-if [ $de = "i3" ]; then
-	arch-chroot /mnt systemctl enable tlp.service
-	arch-chroot /mnt systemctl enable NetworkManager.service
-	arch-chroot /mnt systemctl enable bumblebeed.service
-	arch-chroot /mnt systemctl enable lightdm.service
-	arch-chroot /mnt systemctl enable firewalld.service
-elif [ $de = "Deepin" ]; then
-	arch-chroot /mnt systemctl enable tlp.service
-	arch-chroot /mnt systemctl enable NetworkManager.service
-	arch-chroot /mnt systemctl enable lightdm.service
-	arch-chroot /mnt systemctl enable firewalld.service
-else
-	arch-chroot /mnt systemctl enable tlp.service
-	arch-chroot /mnt systemctl enable NetworkManager.service
-	arch-chroot /mnt systemctl enable gdm.service
-	arch-chroot /mnt systemctl enable firewalld.service
-fi
+arch-chroot /mnt systemctl enable tlp.service
+arch-chroot /mnt systemctl enable NetworkManager.service
+arch-chroot /mnt systemctl enable bumblebeed.service
+arch-chroot /mnt systemctl enable lightdm.service
+arch-chroot /mnt systemctl enable firewalld.service
 
-# Making i3 default for startx (only if i3 is DE)
-if [ $de = "i3" ]; then
-	arch-chroot /mnt echo "exec i3" >> /mnt/root/.xinitrc
-	arch-chroot /mnt echo "exec i3" > /mnt/home/${username}/.xinitrc
-fi
+# Making i3 default for startx
+arch-chroot /mnt echo "exec i3" >> /mnt/root/.xinitrc
+arch-chroot /mnt echo "exec i3" > /mnt/home/${username}/.xinitrc
 
 # Makepkg optimization
 arch-chroot /mnt sed -i -e 's/#MAKEFLAGS="-j2"/MAKEFLAGS=-j'$(nproc --ignore 1)'/' -e 's/-march=x86-64 -mtune=generic/-march=native/' -e 's/xz -c -z/xz -c -z -T '$(nproc --ignore 1)'/' /etc/makepkg.conf
@@ -230,52 +166,44 @@ arch-chroot /mnt sudo -u ${username} git clone https://aur.archlinux.org/yay.git
 arch-chroot /mnt sudo -u ${username} cd /home/${username}/yay_tmp_install && yes | makepkg -si
 arch-chroot /mnt rm -rf /home/${username}/yay_tmp_install
 
-if [ $de = "i3" ]; then
-	# Installing i3-gaps and polybar
-	arch-chroot /mnt sudo -u ${username} yay -S --noconfirm i3-gaps 
-	arch-chroot /mnt sudo -u ${username} yay -S --noconfirm polybar
-	arch-chroot /mnt sudo -u ${username} yay -S --noconfirm brave-bin
-	arch-chroot /mnt sudo -u ${username} yay -S --noconfirm otf-font-awesome
 
-	# Installing fonts
-	arch-chroot /mnt sudo -u ${username} mkdir /home/${username}/fonts_tmp_folder
-	arch-chroot /mnt sudo -u ${username} sudo mkdir /usr/share/fonts/OTF/
+# Installing i3-gaps and polybar
+arch-chroot /mnt sudo -u ${username} yay -S --noconfirm i3-gaps 
+arch-chroot /mnt sudo -u ${username} yay -S --noconfirm polybar
+arch-chroot /mnt sudo -u ${username} yay -S --noconfirm brave-bin
+arch-chroot /mnt sudo -u ${username} yay -S --noconfirm otf-font-awesome
 
-	# Material font
-	arch-chroot /mnt sudo -u ${username} "cd /home/${username}/fonts_tmp_folder && wget https://github.com/adi1090x/polybar-themes/blob/master/polybar-8/fonts/Material.ttf"
-	arch-chroot /mnt sudo -u ${username} "sudo cp /home/${username}/fonts_tmp_folder/Material.ttf /usr/share/fonts/OTF/"
-	# Iosevka font
-	arch-chroot /mnt sudo -u ${username} "cd /home/${username}/fonts_tmp_folder && wget https://github.com/adi1090x/polybar-themes/blob/master/polybar-8/fonts/iosevka-regular.ttf"
-	arch-chroot /mnt sudo -u ${username} "sudo cp /home/${username}/fonts_tmp_folder/iosevka-regular.ttf /usr/share/fonts/OTF/"
-	# Meslo for powerline font
-	arch-chroot /mnt sudo -u ${username} "cd /home/${username}/fonts_tmp_folder && wget https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf"
-	arch-chroot /mnt sudo -u ${username} "sudo cp /home/${username}/fonts_tmp_folder/Meslo\ LG\ M\ Regular\ for\ Powerline.ttf /usr/share/fonts/OTF/"
-	# Removing fonts tmp folder
-	arch-chroot /mnt sudo -u ${username} rm -rf /home/${username}/fonts_tmp_folder
+# Installing fonts
+arch-chroot /mnt sudo -u ${username} mkdir /home/${username}/fonts_tmp_folder
+arch-chroot /mnt sudo -u ${username} sudo mkdir /usr/share/fonts/OTF/
 
-	# Installing configs
-	arch-chroot /mnt sudo -u ${username} mkdir /home/${username}/GitHub
-	arch-chroot /mnt sudo -u ${username} git clone https://github.com/zetaemme/dotfiles /home/${username}/GitHub/dotfiles
-	arch-chroot /mnt sudo -u ${username} git clone https://github.com/zetaemme/zls /home/${username}/GitHub/zls
-	arch-chroot /mnt sudo -u ${username} "chmod +x /home/${username}/GitHub/zls/install_configs.sh"
-	arch-chroot /mnt sudo -u ${username} /bin/zsh -c "cd /home/${username}/GitHub/zls && ./install_configs.sh"
+# Material font
+arch-chroot /mnt sudo -u ${username} "cd /home/${username}/fonts_tmp_folder && wget https://github.com/adi1090x/polybar-themes/blob/master/polybar-8/fonts/Material.ttf"
+arch-chroot /mnt sudo -u ${username} "sudo cp /home/${username}/fonts_tmp_folder/Material.ttf /usr/share/fonts/OTF/"
+# Iosevka font
+arch-chroot /mnt sudo -u ${username} "cd /home/${username}/fonts_tmp_folder && wget https://github.com/adi1090x/polybar-themes/blob/master/polybar-8/fonts/iosevka-regular.ttf"
+arch-chroot /mnt sudo -u ${username} "sudo cp /home/${username}/fonts_tmp_folder/iosevka-regular.ttf /usr/share/fonts/OTF/"
+# Meslo for powerline font
+arch-chroot /mnt sudo -u ${username} "cd /home/${username}/fonts_tmp_folder && wget https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf"
+arch-chroot /mnt sudo -u ${username} "sudo cp /home/${username}/fonts_tmp_folder/Meslo\ LG\ M\ Regular\ for\ Powerline.ttf /usr/share/fonts/OTF/"
+# Removing fonts tmp folder
+arch-chroot /mnt sudo -u ${username} rm -rf /home/${username}/fonts_tmp_folder
 
-	# Setting lightdm greeter
-	arch-chroot /mnt sudo -u ${username} sed -i '102s/^#.*greeter-session=/s/^#//' /etc/lightdm/lightdm.conf
-	arch-chroot /mnt sudo -u ${username} sed -i '102s/^greeter-session=/ s/$/lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
+# Installing configs
+arch-chroot /mnt sudo -u ${username} mkdir /home/${username}/GitHub
+arch-chroot /mnt sudo -u ${username} git clone https://github.com/zetaemme/dotfiles /home/${username}/GitHub/dotfiles
+arch-chroot /mnt sudo -u ${username} git clone https://github.com/zetaemme/zls /home/${username}/GitHub/zls
+arch-chroot /mnt sudo -u ${username} "chmod +x /home/${username}/GitHub/zls/install_configs.sh"
+arch-chroot /mnt sudo -u ${username} /bin/zsh -c "cd /home/${username}/GitHub/zls && ./install_configs.sh"
 
-	arch-chroot /mnt sudo -u ${username} sed -i '111s/^#.*session-startup-script=/s/^#//' /etc/lightdm/lightdm.conf
-	arch-chroot /mnt sudo -u ${username} sed -i '111s/^session-startup-script=/ s/$//home/zetaemme/.fehbg' /etc/lightdm/lightdm.conf
+# Setting lightdm greeter
+arch-chroot /mnt sudo -u ${username} sed -i '102s/^#.*greeter-session=/s/^#//' /etc/lightdm/lightdm.conf
+arch-chroot /mnt sudo -u ${username} sed -i '102s/^greeter-session=/ s/$/lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
 
-	arch-chroot /mnt sudo -u ${username} sed -i '21s/^webkit_theme/ s/$/ litarvan' /etc/lightdm/lightdm-webkit2-greeter.conf
-else
-	arch-chroot /mnt sudo -u ${username} yay -S --noconfirm brave-bin
-	
-	if [ $de = "Deepin" ]; then
-		arch-chroot /mnt sudo -u ${username} sed -i '102s/^#.*greeter-session=/s/^#//' /etc/lightdm/lightdm.conf
-		arch-chroot /mnt sudo -u ${username} sed -i '102s/^greeter-session=/ s/$/lightdm-deepin-greeter/' /etc/lightdm/lightdm.conf
-	fi
-fi
+arch-chroot /mnt sudo -u ${username} sed -i '111s/^#.*session-startup-script=/s/^#//' /etc/lightdm/lightdm.conf
+arch-chroot /mnt sudo -u ${username} sed -i '111s/^session-startup-script=/ s/$//home/zetaemme/.fehbg' /etc/lightdm/lightdm.conf
+
+arch-chroot /mnt sudo -u ${username} sed -i '21s/^webkit_theme/ s/$/ litarvan' /etc/lightdm/lightdm-webkit2-greeter.conf
 
 # Unmounting all mounted partitions
 umount -R /mnt
